a6e7408dc6da964fd49289a5c3a2cd44490f886f
Shinki Nishikori <nishi@ruse.jp>
2025-04-30 09:58:33 +0900

    Fix warnings

--- demacbin.c
+++ demacbin.c
@@ -1,20 +1,18 @@
-#include <sys/types.h>
-#include <sys/param.h>
 #include <fcntl.h>
+#include <sys/param.h>
+#include <sys/types.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+int main() {
+  u_char buf[256];
+  int len;
 
-int main()
-{
-	u_char	buf[256];
-	int	len;
-
-	if ((len = read(0, buf, 128)) < 0)
-		exit(1);
-	while ((len = read(0, buf, sizeof(buf))) > 0)
-		write(1, buf, len);
-	exit(0);
+  if ((len = read(0, buf, 128)) < 0)
+    exit(1);
+  while ((len = read(0, buf, sizeof(buf))) > 0)
+    write(1, buf, len);
+  exit(0);
 }
